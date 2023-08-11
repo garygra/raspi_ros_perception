07646e095f3b0511e11f4d325ff99e8a33c73f55
Edgar Granados <gary.granados@gmail.com>
2023-08-10 19:53:32 -0700

    printing cam params

--- src/perception/executables/framerate_check.cpp
+++ src/perception/executables/framerate_check.cpp
@@ -54,10 +54,10 @@ int main(int argc, char** argv)
   cap.set(cv::CAP_PROP_AUTO_WB, 1);
   cap.set(cv::CAP_PROP_AUTO_EXPOSURE, 0);
 
-  ROS_DEBUG_STREAM("Camera: " << camera_file);
-  ROS_DEBUG_STREAM("cv::CAP_PROP_FRAME_WIDTH: " << cap.get(cv::CAP_PROP_FRAME_WIDTH));
-  ROS_DEBUG_STREAM("cv::CAP_PROP_FRAME_HEIGHT: " << cap.get(cv::CAP_PROP_FRAME_HEIGHT));
-  ROS_DEBUG_STREAM("cv::CAP_PROP_FPS: " << cap.get(cv::CAP_PROP_FPS));
+  std::cout << "Camera: " << camera_file);
+  std::cout << "cv::CAP_PROP_FRAME_WIDTH: " << cap.get(cv::CAP_PROP_FRAME_WIDTH));
+  std::cout << "cv::CAP_PROP_FRAME_HEIGHT: " << cap.get(cv::CAP_PROP_FRAME_HEIGHT));
+  std::cout << "cv::CAP_PROP_FPS: " << cap.get(cv::CAP_PROP_FPS));
 
   if (!cap.isOpened())
   {
